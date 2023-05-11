86d55f13a9448c7fdb2588f22288e06d6aaea256
Dylan Molina <86082012+dylanintech@users.noreply.github.com>
2023-05-10 22:37:39 -0500

    fixed root page nav

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,11 @@ import Plugins from './plugins/plugins'
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
+    <main className="flex min-h-screen flex-col items-center justify-between p-10 bg-white">
 
-        <div className='flex flex-row items-center justify-between'>
-          <div className='flex flex-row items-center'>
-            <img src='https://media.discordapp.net/attachments/993733319386730541/1105874447057027223/Untitled_design.png?width=1000&height=1000' className='rounded-full w-20 h-20'></img>
+        <div className='flex flex-row items-center justify-between my-2 w-full'>
+          <div className='flex flex-row items-center gap-1'>
+            <img src='https://media.discordapp.net/attachments/993733319386730541/1105874447057027223/Untitled_design.png?width=1000&height=1000' className='border-2 rounded-full w-10 h-10'></img>
             <h1 className='text-xl font-mono text-black'>autoplugins</h1>
           </div>
           <Buttons />
